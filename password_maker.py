823b4585c22272e12dd74b04d5a1e9b4c6f20e9b
areyeuokeianne <annelizabetharyeuokei@gmail.com>
2024-09-17 19:57:20 +0800

    add security questions

--- password_maker.py
+++ password_maker.py
@@ -2,7 +2,19 @@
 import hashlib
 
 def secret_password_maker():
-    questions = []
+    questions = [
+                 "What is Ann's Date of Birth (DD-MM-YYYY)?",
+                 "What is Ann's Nationality?",
+                 "What is Ann's Favorite Holiday?",
+                 "Where does Ann Currently Work?", 
+                 "From What University did Ann Graduate?",
+                 "What City do Ann's Parents Live In?",
+                 "What City are Ann and Jeanie based in?",
+                 "What Place do Ann and Jeanie Visit Every Saturday?",
+                 "What is Ann's Official Email Account?", # u can't find our emails from github right...  
+                 "What is Ann's Throwaway Email Account?",
+                 "What Food is Ann Allergic to?"
+                 ]
     m = hashlib.sha256()
     for question in questions:
         x = input(question+'\n').lower().strip()
