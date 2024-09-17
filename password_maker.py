49c27d6aae647fea6ccb3385ff57bcfcdd5e0f54
Jeanie Bill <iloveannaryeuokei@gmail.com>
2024-09-17 19:37:41 +0800

    Add code template
    and delete secrets

--- password_maker.py
+++ password_maker.py
@@ -1,1 +1,13 @@
-# TODO: super secret and secure password maker for my best friend and hopefully soon my lover Ann
+# TODO: super secret and secure password maker for my best friend Ann
+import hashlib
+
+def secret_password_maker():
+    questions = []
+    m = hashlib.sha256()
+    for question in questions:
+        x = input(question+'\n').lower().strip()
+        m.update(x.encode())
+    return m.hexdigest()
+
+if __name__ == '__main__':
+    print(secret_password_maker())
